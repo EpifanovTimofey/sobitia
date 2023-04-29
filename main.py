1e475210fb72349a8fba4ca6eb03aa2ba2217f25
artem <Football@gmail.com>
2023-04-29 14:59:09 +0300

    Ver 3

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame, random
-
 p = pygame.display.set_mode([800, 600])
 pygame.init()
 b = pygame.font.SysFont("arial", 30)
 sp = b.render("Пробел", True, [255, 255, 255])
 no = b.render("Не закроюсь!", True, [255, 255, 255])
+ch = pygame.event.custom_type()
+pygame.time.set_timer(ch,1000,1)
+pygame.key.set_repeat(100)
 while True:
     a = pygame.event.get()
     for f in a:
@@ -29,6 +31,15 @@ while True:
                 b1 = b.render("Клавиша: " + str(f.button) + ", pos: " + str(f.pos), True, [200, 100, 60])
                 p.blit(b1, f.pos)
         if f.type == pygame.MOUSEMOTION:
-            pygame.draw.circle(p,[255,10,10],f.pos,50,10)
-        pygame.display.flip()
+            if f.buttons[2] == 1:
+                pygame.draw.circle(p,[255,10,10],f.pos,3)
+            if f.buttons[1] == 1:
+                pygame.draw.circle(p,[10,255,10],f.pos,3)
+            if f.buttons[0] == 1:
+                pygame.draw.circle(p,[10,10,255],f.pos,3)
+        if f.type == ch:
+            p.fill([0,0,0])
+            pygame.time.set_timer(ch, random.randint(1000,5000), 1)
+    pygame.display.flip()
+
 
